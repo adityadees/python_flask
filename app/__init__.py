b4b2e46da87e11679464fadcdee6a3bedb9862d0
AdityaDS <adityads@ymail.com>
2020-12-31 19:10:40 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import app_config
+from flas_login import LoginManager
 
-app = Flask(__name__, instance_relative_config=True)
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-from app import views
+def create_app(config_name) : 
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_object(app_config[config_name])
+	app.config.from_pyfile('config.py')
+	#db_init_app(app)
+	@app.route('/')
 
-app.config.from_object('config')
+def hello_world():
+	return 'Hello, World!'
+
+def create_app(config_name):
+	login_manageer.init_app(app)
+	login_manager.login_message = "You must be logged in to acces this page."
+	login_manager.login_view = "auth.login"
+	return app
